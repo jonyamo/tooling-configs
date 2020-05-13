e9f6c9f729c9cb85c172f0fcce62add2b2620388
J.C. Yamokoski <jonyamo@users.noreply.github.com>
2020-05-13 18:20:23 -0400

    Add react config

---
+++ packages/eslint-config-jonyamo-react/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: [
+        'jonyamo-base',
+        'plugin:jsx-a11y/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier/react',
+    ],
+    env: {
+        browser: true,
+    },
+};
